4641efda2a2175cffb9f476e9564118ae0ea7912
Louis THEVENET <louis.tvnt@gmail.Com>
2023-08-17 13:24:14 +0200

    cleaned up py script

--- fetch_airpods_battery.py
+++ fetch_airpods_battery.py
@@ -7,11 +7,21 @@ AIRPODS_MANUFACTURER = 76
 AIRPODS_DATA_LENGTH = 54
 UPDATE_INTERVAL = 2
 
-# Return if left and right is flipped in the data
+async def get_data_from_bluetooth():
+    discovered_devices_and_advertisement_data = await BleakScanner.discover(return_adv=True)
+    for key, dev_and_adv_dat in discovered_devices_and_advertisement_data.items():
+        device = dev_and_adv_dat[0]
+        adv_dat = dev_and_adv_dat[1]
+        if AIRPODS_MANUFACTURER in adv_dat.manufacturer_data.keys():
+            hexa_data = hexlify(adv_dat.manufacturer_data[AIRPODS_MANUFACTURER])
+            if (len(hexa_data)==AIRPODS_DATA_LENGTH):
+                return hexa_data
+    return None
+
 def is_flipped(raw):
     return (int("" + chr(raw[10]), 16) & 0x02) == 0
 
-def add_color(status):
+def add_color_zsh_prompt(status):
     if status=='🚫':
         return status
     if status < 25:
@@ -24,72 +34,36 @@ def add_color(status):
         return f"%{{$fg[yellow]%}}{status}%{{$reset_color%}}"
     else:
         return f"%{{$fg[green]%}}{status}%{{$reset_color%}}"
-
-async def get_data():
-    discovered_devices_and_advertisement_data = await BleakScanner.discover(return_adv=True)
-    for key, dev_and_adv_dat in discovered_devices_and_advertisement_data.items():
-        device = dev_and_adv_dat[0]
-        adv_dat = dev_and_adv_dat[1]
-        if AIRPODS_MANUFACTURER in adv_dat.manufacturer_data.keys():
-            hexa_data = hexlify(adv_dat.manufacturer_data[AIRPODS_MANUFACTURER])
-            if (len(hexa_data)==AIRPODS_DATA_LENGTH):
-                return hexa_data
-    return None
             
-def process_hexa(data_hexa):
-    print(data_hexa)
+def get_battery_from_data(data_hexa):
     flip: bool = is_flipped(data_hexa)
-
-    # On 7th position we can get AirPods model, gen1, gen2, Pro or Max
-    if chr(data_hexa[7]) == '4':
-        model = "AirPodsPro2"
-    elif chr(data_hexa[7]) == 'e':
-        model = "AirPodsPro"
-    elif chr(data_hexa[7]) == '3':
-        model = "AirPods3"
-    elif chr(data_hexa[7]) == 'f':
-        model = "AirPods2"
-    elif chr(data_hexa[7]) == '2':
-        model = "AirPods1"
-    elif chr(data_hexa[7]) == 'a':
-        model = "AirPodsMax"
-    else:
-        model = "unknown"
-
 
     # 0-10 : battery, 15 : disconnected
     status_tmp = int("" + chr(data_hexa[12 if flip else 13]), 16)
     left_status = (100 if status_tmp == 10 else (status_tmp * 10 + 5 if status_tmp <= 10 else '🚫'))
-    print("left status : ",status_tmp)
 
 
     status_tmp = int("" + chr(data_hexa[13 if flip else 12]), 16)
     right_status = (100 if status_tmp == 10 else (status_tmp * 10 + 5 if status_tmp <= 10 else '🚫'))
-    print("right status : ",status_tmp)
-
 
     status_tmp = int("" + chr(data_hexa[15]), 16)
-    print("case status : ",status_tmp)
     case_status = (100 if status_tmp == 10 else (status_tmp  * 10 + 5 if status_tmp <= 10 else '🚫'))
 
-    # On 14th position we can get charge status of AirPods
     charging_status = int("" + chr(data_hexa[14]), 16)
     charging_left:bool = (charging_status & (0b00000010 if flip else 0b00000001)) != 0
     charging_right:bool = (charging_status & (0b00000001 if flip else 0b00000010)) != 0
     charging_case:bool = (charging_status & 0b00000100) != 0
 
-    return f"L:{add_color(left_status)} R:{add_color(right_status)} C:{add_color(case_status)}"
+    return "L:"+add_color_zsh_prompt(left_status)+'⚡ 'if charging_left else ' '+ "R:"+add_color_zsh_prompt(right_status)+'⚡ 'if charging_right else ' '+"C:"+add_color_zsh_prompt(case_status)+'⚡'if charging_left else ''
 
 async def main():
-    with open("/tmp/airpods_battery.out", 'w') as writer:
+    with open("/tmp/airpods_battery.out", 'w+') as writer:
         while True:
-            res=await get_data()
+            res=await get_data_from_bluetooth()
             writer.seek(0)
             if res is not None:
-                writer.write(process_hexa(res))
+                writer.write(get_battery_from_data(res))
             writer.truncate()
             sleep(UPDATE_INTERVAL)
-
-
 
 asyncio.run(main())
