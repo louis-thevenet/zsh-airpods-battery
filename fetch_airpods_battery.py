7ca13d6d1628cde125ec569cfb026b1042a25380
Louis THEVENET <louis.tvnt@gmail.Com>
2023-08-17 13:11:36 +0200

    added fancy string

--- fetch_airpods_battery.py
+++ fetch_airpods_battery.py
@@ -5,11 +5,25 @@ from time import sleep
 
 AIRPODS_MANUFACTURER = 76
 AIRPODS_DATA_LENGTH = 54
-UPDATE_INTERVAL = 1
+UPDATE_INTERVAL = 2
 
 # Return if left and right is flipped in the data
 def is_flipped(raw):
     return (int("" + chr(raw[10]), 16) & 0x02) == 0
+
+def add_color(status):
+    if status=='🚫':
+        return status
+    if status < 25:
+        return f"%{{$fg[red]%}}{status}%{{$reset_color%}}"
+
+    elif status < 50:
+        return f"%{{$fg[orange]%}}{status}%{{$reset_color%}}"
+
+    elif status < 75:
+        return f"%{{$fg[yellow]%}}{status}%{{$reset_color%}}"
+    else:
+        return f"%{{$fg[green]%}}{status}%{{$reset_color%}}"
 
 async def get_data():
     discovered_devices_and_advertisement_data = await BleakScanner.discover(return_adv=True)
@@ -23,6 +37,7 @@ async def get_data():
     return None
             
 def process_hexa(data_hexa):
+    print(data_hexa)
     flip: bool = is_flipped(data_hexa)
 
     # On 7th position we can get AirPods model, gen1, gen2, Pro or Max
@@ -41,17 +56,21 @@ def process_hexa(data_hexa):
     else:
         model = "unknown"
 
-    # Checking left AirPod for availability and storing charge in variable
+
+    # 0-10 : battery, 15 : disconnected
     status_tmp = int("" + chr(data_hexa[12 if flip else 13]), 16)
-    left_status = (100 if status_tmp == 10 else (status_tmp * 10 + 5 if status_tmp <= 10 else -1))
+    left_status = (100 if status_tmp == 10 else (status_tmp * 10 + 5 if status_tmp <= 10 else '🚫'))
+    print("left status : ",status_tmp)
 
-    # Checking right AirPod for availability and storing charge in variable
+
     status_tmp = int("" + chr(data_hexa[13 if flip else 12]), 16)
-    right_status = (100 if status_tmp == 10 else (status_tmp * 10 + 5 if status_tmp <= 10 else -1))
+    right_status = (100 if status_tmp == 10 else (status_tmp * 10 + 5 if status_tmp <= 10 else '🚫'))
+    print("right status : ",status_tmp)
 
-    # Checking AirPods case for availability and storing charge in variable
+
     status_tmp = int("" + chr(data_hexa[15]), 16)
-    case_status = (100 if status_tmp == 10 else (status_tmp * 10 + 5 if status_tmp <= 10 else -1))
+    print("case status : ",status_tmp)
+    case_status = (100 if status_tmp == 10 else (status_tmp  * 10 + 5 if status_tmp <= 10 else '🚫'))
 
     # On 14th position we can get charge status of AirPods
     charging_status = int("" + chr(data_hexa[14]), 16)
@@ -59,32 +78,17 @@ def process_hexa(data_hexa):
     charging_right:bool = (charging_status & (0b00000001 if flip else 0b00000010)) != 0
     charging_case:bool = (charging_status & 0b00000100) != 0
 
-    
-    return dict(
-        status=1,
-        charge=dict(
-            left=left_status,
-            right=right_status,
-            case=case_status
-        ),
-        charging_left=charging_left,
-        charging_right=charging_right,
-        charging_case=charging_case,
-        model=model,
-        raw=data_hexa.decode("utf-8")
-    )
+    return f"L:{add_color(left_status)} R:{add_color(right_status)} C:{add_color(case_status)}"
 
 async def main():
-    with open("/tmp/airpods_battery.out", 'w+') as writer:
+    with open("/tmp/airpods_battery.out", 'w') as writer:
         while True:
             res=await get_data()
             writer.seek(0)
             if res is not None:
-                writer.write("works")
-            else:
-                writer.write("works2")
+                writer.write(process_hexa(res))
             writer.truncate()
-        sleep(UPDATE_INTERVAL)
+            sleep(UPDATE_INTERVAL)
 
 
 
